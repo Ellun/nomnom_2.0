49e8e2ba80a69fa2e6aca94837514bbc6b39c038
Elton Cheng <eltoncheng86@gmail.com>
2016-05-06 20:56:09 -0400

    added imput and button component

---
+++ public/js/components/input.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class Input extends Component {
+  render() {
+    return <input type={this.props.type} placeholder={this.props.placeholder} />
+  }
+}
